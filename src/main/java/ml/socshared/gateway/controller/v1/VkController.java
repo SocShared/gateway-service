266614edff9a5de6e5cbde26c2dc9cfc76ca14db
nrx <ageevav97@gmail.com>
2020-06-04 15:27:18 +0300

    Поправил подключения аккунта vk к системе

--- src/main/java/ml/socshared/gateway/controller/v1/VkController.java
+++ src/main/java/ml/socshared/gateway/controller/v1/VkController.java
@@ -1,6 +1,7 @@
 package ml.socshared.gateway.controller.v1;
 
 
+import lombok.extern.slf4j.Slf4j;
 import ml.socshared.gateway.api.v1.rest.VkApi;
 import ml.socshared.gateway.client.VkServiceClient;
 import ml.socshared.gateway.domain.response.vk_adapter.GroupResponse;
@@ -15,6 +16,7 @@ import java.util.UUID;
 
 @RequestMapping("/api/v1")
 @RestController
+@Slf4j
 public class VkController implements VkApi {
 
     VkServiceClient client;
@@ -25,9 +27,10 @@ public class VkController implements VkApi {
 
 
     @Override
-    @PostMapping("/protected/social/vk/app/")
+    @PostMapping("/protected/social/vk/app")
     public SuccessResponse setToken( @RequestBody String accessToken,
                                     @RequestHeader(value = "Authorization", required = false) String token) {
+       log.info("Request add vk app");
         UUID systemUserId = UUID.fromString("75853635-c91a-492b-9276-2b7d3511e0de");
         token = "RequiredToken!";
         client.appRegister(systemUserId, accessToken, token);
