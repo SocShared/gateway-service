388aded1a812e9259408c9fe1c8c40ab58b5cdc9
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-08 00:05:45 +0300

    Изменил метод получения keywords c GET на POST

--- src/main/java/ml/socshared/gateway/client/TextAnalyzerClient.java
+++ src/main/java/ml/socshared/gateway/client/TextAnalyzerClient.java
@@ -3,17 +3,14 @@ package ml.socshared.gateway.client;
 import ml.socshared.gateway.domain.text_analyzer.request.TextRequest;
 import ml.socshared.gateway.domain.text_analyzer.response.KeyWordResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "text-analyzer", url = "${feign.url.text}")
 public interface TextAnalyzerClient {
 
-    @GetMapping("/api/v1/private/key_words")
+    @PostMapping("/api/v1/private/key_words")
     List<KeyWordResponse> extractKeyWords(@RequestBody TextRequest text,
                                                  @RequestParam(value = "min_len", defaultValue = "2") Integer minLength,
                                                  @RequestParam(value = "max_len", defaultValue  = "4") Integer maxLength,
