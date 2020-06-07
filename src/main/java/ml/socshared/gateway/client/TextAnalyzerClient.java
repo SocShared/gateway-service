87e1f28ece017444381da631c98dd8713ff1399f
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-07 23:41:29 +0300

    Добавил TextRequest

--- src/main/java/ml/socshared/gateway/client/TextAnalyzerClient.java
+++ src/main/java/ml/socshared/gateway/client/TextAnalyzerClient.java
@@ -1,5 +1,6 @@
 package ml.socshared.gateway.client;
 
+import ml.socshared.gateway.domain.text_analyzer.request.TextRequest;
 import ml.socshared.gateway.domain.text_analyzer.response.KeyWordResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ import java.util.List;
 public interface TextAnalyzerClient {
 
     @GetMapping("/api/v1/private/key_words")
-    List<KeyWordResponse> extractKeyWords(@RequestBody String text,
+    List<KeyWordResponse> extractKeyWords(@RequestBody TextRequest text,
                                                  @RequestParam(value = "min_len", defaultValue = "2") Integer minLength,
                                                  @RequestParam(value = "max_len", defaultValue  = "4") Integer maxLength,
                                                  @RequestHeader("Authorization") String token);
---
+++ src/main/java/ml/socshared/gateway/domain/text_analyzer/request/TextRequest.java
@@ -0,0 +1,14 @@
+package ml.socshared.gateway.domain.text_analyzer.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TextRequest {
+
+    private String text;
+
+}
